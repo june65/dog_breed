ba4f983a1a09a3b00f74d9878f47319325193387
june0605 <june0100726@gmail.com>
2022-08-09 13:45:58 +0900

    Issue(#1)Add Model_v2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import glob
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -10,11 +12,12 @@ session = InteractiveSession(config=config)
 
 import tensorflow as tf
 
-
 model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
   tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
   tf.keras.layers.MaxPooling2D(2,2),
   tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
   tf.keras.layers.MaxPooling2D(2,2),
@@ -36,7 +39,14 @@ model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['
 
 from keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(rescale = 1.0/255.)
+train_datagen = ImageDataGenerator(rescale = 1.0/255.,
+                                 rotation_range=40,
+                                 width_shift_range=0.2,
+                                 height_shift_range=0.2,
+                                 shear_range=0.2,
+                                 zoom_range=0.2,
+                                 horizontal_flip=True,
+                                 fill_mode='nearest')
 
 train_generator = train_datagen.flow_from_directory(
   'C:/Users/이준명/Desktop/dog_breed/data/data2/train',
@@ -46,6 +56,7 @@ train_generator = train_datagen.flow_from_directory(
 )
 
 test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
 validation_generator = train_datagen.flow_from_directory(
   'C:/Users/이준명/Desktop/dog_breed/data/data2/test',
   target_size=(150, 150),
@@ -56,11 +67,11 @@ history = model.fit(
   train_generator,
   validation_data=validation_generator,
   steps_per_epoch=20,
-  epochs=100,
+  epochs=500,
   verbose=1
 )
 
-model.save('./dog_breed_model2')
+model.save('./dog_breed_model')
 
 import matplotlib.pyplot as plt
 
